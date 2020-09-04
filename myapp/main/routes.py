d23476a98ce7f84a0627f39684b436092aff8878
Josh Loesch <jloesch30@gmail.com>
2020-09-04 00:14:50 -0500

    pyrebase stuff

--- myapp/main/routes.py
+++ myapp/main/routes.py
@@ -1,11 +1,20 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template, request, session
+from myapp import auth, firebase
 
 main = Blueprint('main', __name__)
 
-
-@main.route('/')
+@main.route('/', methods=['POST', 'GET'])
 def index():
-    return redirect(url_for('main.home'))
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+
+        user = auth.create_user_with_email_and_password(email, password)
+        session['user'] = user
+        print(session)
+        return redirect(url_for('users.signout', user=user))
+    else:
+        return render_template("index.html")
 
 @main.route('/home')
 def home():
--- myapp/users/routes.py
+++ myapp/users/routes.py
@@ -1,3 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect, url_for, render_template, session
+from myapp import auth,firebase
+
 
 users = Blueprint('users', __name__)
+
+@users.route('/signout', methods=['POST', 'GET'])
+def signout():
+    user = session['user']
+    if user:
+        user = session['user']
+        user.signOut()
+        print('signed out')
+        return 'user signed out'
+    else:
+        return 'user not found'
